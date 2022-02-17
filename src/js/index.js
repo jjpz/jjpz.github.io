711442bf013be1ee6c9c379a69feb75804a14dd9
Juanjo Perez <jjperez85@gmail.com>
2022-02-16 19:31:53 -0500

    add parcel build tool and edit content

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+// footer year
+const year = new Date().getFullYear();
+let footerYear = document.querySelector('.year');
+footerYear.innerHTML = year;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/index.html",
+    "./src/js/main.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  prefix: "tw-"
+}
