5317958e60eaf808d2246d79659603dbcc605f4e
Juanjo Perez <jjperez85@gmail.com>
2022-03-01 21:49:58 -0500

    full redesign

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,10 @@
-// footer year
+// scroll to contact section
+document.querySelector('.available a').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector('#contact').scrollIntoView({behavior: 'smooth'});
+});
+
+// display current year in footer
 const year = new Date().getFullYear();
 let footerYear = document.querySelector('.year');
 footerYear.innerHTML = year;
