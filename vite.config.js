a4e0df01c6fcd1537c1dd8b0df9940a8e16c26ce
Juan Jose Perez <jjperez85@gmail.com>
2025-09-30 15:40:25 -0400

    simplify vite config

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  root: 'src',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true
-  },
-  server: {
-    open: true
-  },
   plugins: [
     tailwindcss()
   ]
