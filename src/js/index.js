d69a47763ff7230abf4ad9325e87dd978b17cb04
Juan Jose Perez <jjperez85@gmail.com>
2025-10-03 16:58:24 -0400

    simplify js

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,2 @@
-// scroll to contact section
-document.querySelector('.available a').addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector('#contact').scrollIntoView({behavior: 'smooth'});
-});
-
 // display current year in footer
-const year = new Date().getFullYear();
-let footerYear = document.querySelector('.year');
-footerYear.innerHTML = year;
+document.querySelector('.js-year').innerHTML = new Date().getFullYear();
