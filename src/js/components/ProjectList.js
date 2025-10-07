63a8330db8131840ddbc48100d2d63e889e5ba2d
Juan Jose Perez <jjperez85@gmail.com>
2025-10-07 15:36:11 -0400

    incorporate external link component into project list component

--- src/js/components/ProjectList.js
+++ src/js/components/ProjectList.js
@@ -1,16 +1,15 @@
 import { ArrowIcon } from './icons.js';
+import { ExternalLink } from './ExternalLink.js';
 
 export function ProjectList(projects) {
     return projects.map(project => `
         <li class="px-2">
-            <a
-                class="link-arrow inline-flex items-center justify-center ps-4 text-link no-underline"
-                href="${project.url}"
-                target="_blank"
-            >
-                <span>${project.name}</span>
-                ${ArrowIcon()}
-            </a>
+            ${ExternalLink({
+                href: project.url,
+                text: project.name,
+                classes: 'link-arrow inline-flex items-center justify-center ps-4 text-link no-underline',
+                children: ArrowIcon()
+            })}
         </li>
     `).join('');
 }
