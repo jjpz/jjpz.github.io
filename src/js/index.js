93d873be885a444585161aa653fdd898816499d0
Juan Jose Perez <jjperez85@gmail.com>
2025-10-07 14:56:11 -0400

    incorporate template-based rendering in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,12 @@
-// display current year in footer
+import experience from '../data/experience.json';
+import projects from '../data/projects.json';
+import skills from '../data/skills.json';
+import { ExperienceList } from './components/ExperienceList.js';
+import { ProjectList } from './components/ProjectList.js';
+
+// Render dynamic content
+document.querySelector('#experience').innerHTML = ExperienceList(experience);
+document.querySelector('#work ul').innerHTML = ProjectList(projects);
+
+// Display current year in footer
 document.querySelector('.js-year').innerHTML = new Date().getFullYear();
