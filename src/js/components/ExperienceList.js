3ce1ea7da80572c63119deb9af728e47cc81ddc1
Juan Jose Perez <jjperez85@gmail.com>
2025-10-07 15:35:57 -0400

    incorporate external link component into experience list component

--- src/js/components/ExperienceList.js
+++ src/js/components/ExperienceList.js
@@ -1,16 +1,15 @@
+import { ExternalLink } from './ExternalLink.js';
+
 export function ExperienceList(experience) {
     return experience.map(job => `
         <article class="flex flex-col gap-y-2">
             <div>
                 <span>${job.position}</span>
                 <span> @ </span>
-                <a
-                    class="link-underline inline-block relative text-link no-underline"
-                    href="${job.url}"
-                    target="_blank"
-                >
-                    <span>${job.company}</span>
-                </a>
+                ${ExternalLink({
+                    href: job.url,
+                    text: job.company
+                })}
             </div>
             <div class="flex flex-wrap items-center justify-center gap-x-2 gap-y-0.5 text-light-green">
                 <span>${job.location}</span>
