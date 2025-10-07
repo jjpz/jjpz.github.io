62677f40a48d6b6db31ffaf6bd32adb91247faf8
Juan Jose Perez <jjperez85@gmail.com>
2025-10-07 16:06:43 -0400

    render social links

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
+import config from '../data/config.json';
 import experience from '../data/experience.json';
 import projects from '../data/projects.json';
-import skills from '../data/skills.json';
+import { SocialLinksList } from './components/SocialLinksList.js';
 import { ExperienceList } from './components/ExperienceList.js';
 import { ProjectList } from './components/ProjectList.js';
 
 // Render dynamic content
+document.querySelector('#links').innerHTML = SocialLinksList(config.socialLinks);
 document.querySelector('#experience').innerHTML = ExperienceList(experience);
 document.querySelector('#work ul').innerHTML = ProjectList(projects);
 
